655c7bed953b41662b99eec81f082b68eab884dc
sreeja-create <alesreeja1@gmail.com>
2026-06-03 16:43:20 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,10 @@
+str.endswith():-
+strl="sreeja"
+print(str.endswith("eja"))
+output:-true
+‐-------------------------------
+str.capitalize():'
+strl="sreeja"
+print(str.capitalize())
+output:-Sreeja.  #only 1st letter changes 
+‐-------------------------------
