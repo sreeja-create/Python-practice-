5b01c9d0d0adfb82d633e97a5d24270d65b80c48
sreeja-create <alesreeja1@gmail.com>
2026-06-08 16:48:39 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -23,6 +23,16 @@ while i<=10
      print(i)
 i=i+1
 ---------------
+factorial of no.
+ 
+n=int(input("n: ")
+fact=1
+i=1
+while i<=n
+   fact=fact*i
+    i=i+1
+   print(fact)
+--------------
 multiplication of 2 table
 num = 1
 
@@ -127,4 +137,11 @@ output:-
 .
 
 1
+---------
+factorial of no.
+n=int(input("n: ")
+fact=1
+for i in range(1,n+1)
+  fact=fact*i
+   print(fact)
 ---------
